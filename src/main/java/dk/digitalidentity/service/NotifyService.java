6379c686002e5c70d52a0d65344f4d01c1e1d8b7
kbp-di <kbp@digital-identity.dk>
2023-12-11 12:12:26 +0100

    restructure notifications

---
+++ src/main/java/dk/digitalidentity/service/NotifyService.java
@@ -0,0 +1,38 @@
+package dk.digitalidentity.service;
+
+import dk.digitalidentity.model.entity.Task;
+import dk.digitalidentity.samlmodule.config.settings.DISAML_Configuration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+@Slf4j
+@Service
+public class NotifyService {
+    private final DISAML_Configuration diSamlConfiguration;
+    private final MailService mailService;
+
+    public NotifyService(final DISAML_Configuration diSamlConfiguration, final MailService mailService) {
+        this.diSamlConfiguration = diSamlConfiguration;
+        this.mailService = mailService;
+    }
+
+    @Transactional
+    public void notifyTask(final Task task) {
+        if (task.getHasNotifiedResponsible() != null && task.getHasNotifiedResponsible()) {
+            log.warn("Task '" + task.getName() + "' already notified");
+            return;
+        }
+        final LocalDate today = LocalDate.now();
+        final String baseUrl = diSamlConfiguration.getSp().getBaseUrl();
+        final String msg = "Din opgave " + task.getName() + " har deadline om " + ChronoUnit.DAYS.between(today, task.getNextDeadline()) + " dag(e).\n" +
+            " Du kan finde opgaven her: " + baseUrl + "/tasks/" +  task.getId();
+        if (mailService.sendMessage(task.getResponsibleUser().getEmail(), "Deadline Notifikation", msg)) {
+            task.setHasNotifiedResponsible(true);
+        }
+    }
+
+}
