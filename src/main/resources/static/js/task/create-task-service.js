5a2468c9891e9298118108dabe90174ac221f62d
Kaspar Bach Pedersen <kbp@digital-identity.dk>
2023-12-27 19:08:04 +0100

    Load create task dialog on click

--- src/main/resources/static/js/task/create-task-service.js
+++ src/main/resources/static/js/task/create-task-service.js
@@ -7,36 +7,65 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function CreateTaskService() {
 
     this.init = function() {
-        this.load();
-        this.showWhenLoaded = false;
     }
 
-    this.load = function() {
+    this.selectCreateTaskOption = function(value) {
+        const form = document.querySelector('#taskCreateForm');
+        const repetitionField = form.querySelector('#repetition');
+        if (value === 'TASK') {
+            repetitionField.value = 'NONE';
+        }
+        repetitionField.disabled = value !== 'CHECK';
+    }
+
+    this.loaded = function() {
+        let self = this;
+        this.selectCreateTaskOption('TASK');
+        initDatepicker("#taskDeadlineBtn", "#taskDeadline");
+
+        this.createTaskUserChoicesEditSelect = initUserSelect('taskUserSelect');
+        this.createTaskOuChoicesEditSelect = initOUSelect('taskOuSelect');
+        this.createTaskUserChoicesEditSelect.passedElement.element.addEventListener('change', function() {
+            checkInputField(self.createTaskUserChoicesEditSelect);
+        });
+        initFormValidationForForm('taskCreateForm',
+            () => validateChoices(
+                this.createTaskUserChoicesEditSelect, this.createTaskOuChoicesEditSelect));
+    }
+
+    this.show = function() {
         this.loading = true;
         fetch(`/tasks/form`)
             .then(response => response.text()
                 .then(data => {
                     document.getElementById('taskFormDialog').innerHTML = data;
-                    sharedTaskFormLoaded();
-                    initCreateTaskRelationSelect();
+                    this.loaded();
+                    this.initTaskRelationSelect();
                     initTagSelect('createTaskTagsSelect');
-                    this.loading = false;
-                    if (this.showWhenLoaded) {
-                        this.showWhenLoaded = false;
-                        this.show();
-                    }
+                    const createTaskModal = new bootstrap.Modal(document.getElementById('taskFormDialog'));
+                    createTaskModal.show();
                 }))
             .catch(error => toastService.error(error))
             .finally(() => this.loading = false);
     }
 
-    this.show = function () {
-        if (this.loading) {
-            this.showWhenLoaded = true;
-        } else {
-            const createTaskModal = new bootstrap.Modal(document.getElementById('taskFormDialog'));
-            createTaskModal.show();
-        }
+    this.initTaskRelationSelect = function() {
+        const relationsSelect = document.getElementById('createTaskRelationsSelect');
+        const relationsChoice = initSelect(relationsSelect);
+        updateRelations(relationsChoice, "");
+        relationsSelect.addEventListener("search",
+            function(event) {
+                updateRelations(relationsChoice, event.detail.value);
+            },
+            false,
+        );
+        relationsSelect.addEventListener("change",
+            function(event) {
+                updateRelations(relationsChoice, "");
+            },
+            false,
+        );
+        this.createTaskRelationSelect = relationsChoice;
     }
 
 }
