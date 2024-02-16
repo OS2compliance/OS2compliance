d5fd8505ad8b77061552b438fd17b56f62f67cb4
Kaspar Bach Pedersen <kbp@digital-identity.dk>
2024-02-16 14:03:52 +0100

    create form

--- src/main/java/dk/digitalidentity/controller/mvc/CatalogController.java
+++ src/main/java/dk/digitalidentity/controller/mvc/CatalogController.java
@@ -1,13 +1,20 @@
 package dk.digitalidentity.controller.mvc;
 
+import dk.digitalidentity.model.entity.ThreatCatalog;
 import dk.digitalidentity.security.RequireUser;
 import dk.digitalidentity.service.CatalogService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @Controller
@@ -20,7 +27,29 @@ public class CatalogController {
     @GetMapping
     public String riskList(final Model model) {
         model.addAttribute("threatCatalogs", threatCatalogService.findAll());
-        return "catalog/index";
+        return "catalogs/index";
+    }
+
+    @GetMapping("form")
+    public String form(final Model model, @RequestParam(name = "id", required = false) final String id) {
+        if (id == null) {
+            model.addAttribute("catalog", new ThreatCatalog());
+            model.addAttribute("formId", "createForm");
+            model.addAttribute("formTitle", "Nyt trusselskatalog");
+        } else {
+            final ThreatCatalog catalog = threatCatalogService.get(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+            model.addAttribute("catalog", catalog);
+            model.addAttribute("formId", "editForm");
+            model.addAttribute("formTitle", "Rediger trusselskatalog");
+        }
+        return "catalogs/form";
+    }
+
+    @Transactional
+    @PostMapping("form")
+    public String formPost(@ModelAttribute final ThreatCatalog supplier) {
+        return "redirect:/catalogs";
     }
 
 }
--- src/main/java/dk/digitalidentity/service/CatalogService.java
+++ src/main/java/dk/digitalidentity/service/CatalogService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +15,10 @@ public class CatalogService {
 
     public List<ThreatCatalog> findAll() {
         return threatCatalogDao.findAll();
+    }
+
+    public Optional<ThreatCatalog> get(final String identifier) {
+        return threatCatalogDao.findById(identifier);
     }
 
 }
