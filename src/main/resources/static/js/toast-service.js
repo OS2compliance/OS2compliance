924f00e7187c5eb5e8e8a4d6feaf5060b38b41aa
Kaspar Bach Pedersen <kbp@digital-identity.dk>
2024-09-09 16:01:31 +0200

    cleanup

--- src/main/resources/static/js/toast-service.js
+++ src/main/resources/static/js/toast-service.js
@@ -4,7 +4,6 @@ const toastService = new ToastService();
 function ToastService() {
 
     this.error = function(message) {
-        console.log(message)
         const container = document.getElementById('toastContainer');
         const template = document.getElementById('toastTemplate');
         const toastElem = template.cloneNode(true);
