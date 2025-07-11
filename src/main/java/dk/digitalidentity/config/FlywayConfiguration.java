917a2ac1bd1e8e298cdddca10095e747767c6b72
julius <jls@digital-identity.dk>
2025-07-11 07:57:08 +0200

    Resolved simplequeue migration war

---
+++ src/main/java/dk/digitalidentity/config/FlywayConfiguration.java
@@ -0,0 +1,31 @@
+package dk.digitalidentity.config;
+
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationVersion;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfiguration {
+
+	@Value("${spring.datasource.driver-class-name}")
+	private String driverClassName;
+
+	@Bean(initMethod = "migrate")
+	public Flyway flyway(final DataSource dataSource) {
+		return Flyway
+				.configure()
+				.configuration(Map.of("driver", driverClassName))
+				.target(MigrationVersion.LATEST)
+				.dataSource(dataSource)
+				.baselineOnMigrate(true)
+				.table("flyway_schema_history")
+				.locations("classpath:/db/migration")
+				.load();
+	}
+}
