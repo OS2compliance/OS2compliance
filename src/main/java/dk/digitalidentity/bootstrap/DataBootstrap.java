d4c14c17b6b307006ed05eef9535626e71ad7d38
Kaspar Bach Pedersen <kbp@digital-identity.dk>
2025-08-07 12:22:01 +0200

    small fixes

--- src/main/java/dk/digitalidentity/bootstrap/DataBootstrap.java
+++ src/main/java/dk/digitalidentity/bootstrap/DataBootstrap.java
@@ -139,9 +139,9 @@ public class DataBootstrap implements ApplicationListener<ApplicationReadyEvent>
 
 			list.get().setCustomizable(true);
 		}
-		settingsService.createSetting(KitosConstants.KITOS_OWNER_ROLE_SETTING_INPUT_FIELD_NAME, "systemejer" , "alias", true);
-		settingsService.createSetting(KitosConstants.KITOS_RESPONSIBLE_ROLE_SETTING_INPUT_FIELD_NAME, "systemansvarlig" , "alias", true);
-		settingsService.createSetting(KitosConstants.KITOS_OPERATION_RESPONSIBLE_ROLE_SETTING_INPUT_FIELD_NAME, "driftsansvarlig" , "alias", true);
+		settingsService.createSetting(KitosConstants.KITOS_OWNER_ROLE_SETTING_INPUT_FIELD_NAME, "systemejer" , "asset", true);
+		settingsService.createSetting(KitosConstants.KITOS_RESPONSIBLE_ROLE_SETTING_INPUT_FIELD_NAME, "systemansvarlig" , "asset", true);
+		settingsService.createSetting(KitosConstants.KITOS_OPERATION_RESPONSIBLE_ROLE_SETTING_INPUT_FIELD_NAME, "driftsansvarlig" , "asset", true);
 	}
 
 	private void seedV24 () {
