a339048aa48cbfd6dea3d4e10ecadbc36a0eb2d3
julius <jls@digital-identity.dk>
2025-08-14 13:17:31 +0200

    dbs oversight list top page buttons tested and fragmentalized

--- src/main/resources/static/js/dbs/dbs-oversight-service.js
+++ src/main/resources/static/js/dbs/dbs-oversight-service.js
@@ -188,7 +188,8 @@ function DBSOversightService() {
         searchService.initSearch(grid, gridConfig);
 
         const customGridFunctions = new CustomGridFunctions(grid, gridDBSOversightUrl, 'assetsDatatable');
-        window.customGridFunctions = customGridFunctions;
+
+        this.initSaveAsExcelButton(customGridFunctions);
 
         gridOptions.init(grid, document.getElementById("gridOptions"));
     }
@@ -211,4 +212,10 @@ function DBSOversightService() {
     this.updateUrl = (prev, query) => {
         return prev + (prev.indexOf('?') >= 0 ? '&' : '?') + new URLSearchParams(query).toString();
     };
+
+    this.initSaveAsExcelButton = (customGridFunctionsObject) => {
+        const saveAsExcelButton = document.getElementById("saveAsExcelButton");
+
+        saveAsExcelButton.addEventListener("click",  () => exportGridServerSide(customGridFunctionsObject, 'DBS_tilsyn'))
+    }
 }
