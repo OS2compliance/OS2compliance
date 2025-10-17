32b66f781255c825ca554f3a09d2c02b725e64e5
Jeremy Gulow <jgu@digital-identity.dk>
2025-10-17 14:03:51 +0200

    Ability to unfold/fold out all threats on riskview

--- src/main/resources/static/js/risk/riskView.js
+++ src/main/resources/static/js/risk/riskView.js
@@ -205,6 +205,31 @@ function validateFieldBeforeSetting(setFieldType, value) {
     return true;
 }
 
+function toggleAllCategories() {
+    const allIcons = document.querySelectorAll('[id^="categoryIcon"]');
+
+    let openCount = 0;
+    let closedCount = 0;
+
+    allIcons.forEach(icon => {
+        if (icon.classList.contains('pli-arrow-down')) {
+            closedCount++;
+        } else {
+            openCount++;
+        }
+    });
+
+    const shouldClose = openCount >= closedCount;
+
+    allIcons.forEach(icon => {
+        const isOpen = icon.classList.contains('pli-arrow-up');
+
+        if ((shouldClose && isOpen) || (!shouldClose && !isOpen)) {
+            icon.click();
+        }
+    });
+}
+
 function updateAverage() {
 
     // probability
@@ -559,6 +584,7 @@ function pageLoaded() {
     for (var i = 0; i < methodSelects.length; i++) {
         methodSelects[i].addEventListener('change', methodSelectChanged, false);
     }
+    document.getElementById('toggleAllCategories').addEventListener('click', toggleAllCategories);
 
     updateAverage();
 
