63b5e2f22776f36c4bafef08d6cc531b6679f74c
Jeremy Gulow <jgu@digital-identity.dk>
2025-10-21 10:31:11 +0200

    protected function added

--- src/main/java/dk/digitalidentity/service/ChoiceValueService.java
+++ src/main/java/dk/digitalidentity/service/ChoiceValueService.java
@@ -5,6 +5,7 @@ import dk.digitalidentity.model.entity.ChoiceValue;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,6 +14,7 @@ public class ChoiceValueService {
 
 	private final ChoiceValueDao choiceValueDao;
 
+	private final List<String> protectedValues = List.of("register-status-not-started-123456", "register-status-in-progress-123456", "register-status-ready-123456");
 
 	public Optional<ChoiceValue> findById(long id) {
 		return choiceValueDao.findById(id);
@@ -28,5 +30,9 @@ public class ChoiceValueService {
 
 	public void delete(ChoiceValue choiceValue) {
 		choiceValueDao.delete(choiceValue);
+	}
+
+	public boolean isProtected(ChoiceValue choiceValue) {
+		return protectedValues.contains(choiceValue.getIdentifier());
 	}
 }
