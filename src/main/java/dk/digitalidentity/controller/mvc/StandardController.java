aed70226f19ade66cf34a1f2333c7f67219045f7
friis jensen <friise2001@googlemail.com>
2025-11-12 13:20:32 +0100

    small fix

--- src/main/java/dk/digitalidentity/controller/mvc/StandardController.java
+++ src/main/java/dk/digitalidentity/controller/mvc/StandardController.java
@@ -394,7 +394,7 @@ public class StandardController {
 
 	@RequireCreateAll
 	@Transactional
-	@PostMapping("/headers/create/{identifier}")g
+	@PostMapping("/headers/create/{identifier}")
 	public String createHeader(@Valid @ModelAttribute final StandardTemplateSection standardTemplateSection, @PathVariable(name = "identifier") final String id, RedirectAttributes redirectAttributes, BindingResult result) {
 		StandardTemplate template = supportingStandardService.lookup(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
