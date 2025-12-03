b9ca0eee3b96398abb727bc85f15fea31e0e7b7d
julius <jls@digital-identity.dk>
2025-12-03 11:01:44 +0100

    Added equals and hashcode to Relation class

--- src/main/java/dk/digitalidentity/model/entity/Relation.java
+++ src/main/java/dk/digitalidentity/model/entity/Relation.java
@@ -16,8 +16,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -30,7 +32,7 @@ import java.util.Set;
 public class Relation {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 
 	@Column(name = "relation_a_id")
 	private Long relationAId;
@@ -51,4 +53,23 @@ public class Relation {
         mappedBy = "relation")
 	@Builder.Default
     private Set<RelationProperty> properties = new HashSet<>();
+
+	@Override
+	public final boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null)
+			return false;
+		Class<?> oEffectiveClass = o instanceof HibernateProxy hibernateProxy ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+		Class<?> thisEffectiveClass = this instanceof HibernateProxy hibernateProxy ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+		if (thisEffectiveClass != oEffectiveClass)
+			return false;
+		Relation relation = (Relation) o;
+		return getId() != null && Objects.equals(getId(), relation.getId());
+	}
+
+	@Override
+	public final int hashCode() {
+		return this instanceof HibernateProxy hibernateProxy ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+	}
 }
