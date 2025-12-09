7ffc0f03130d7c574191a928584258db205afa83
julius <jls@digital-identity.dk>
2025-12-09 09:14:14 +0100

    Changed field in chartconfig to match changes in task

--- src/main/java/dk/digitalidentity/statistic/model/ChartConfiguration/ChartConfigurationDao.java
+++ src/main/java/dk/digitalidentity/statistic/model/ChartConfiguration/ChartConfigurationDao.java
@@ -3,8 +3,11 @@ package dk.digitalidentity.statistic.model.ChartConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChartConfigurationDao extends JpaRepository<ChartConfiguration, Long> {
 
 	List<ChartConfiguration> findAllBySection(String section);
+
+	Optional<ChartConfiguration> findByName(String name);
 }
