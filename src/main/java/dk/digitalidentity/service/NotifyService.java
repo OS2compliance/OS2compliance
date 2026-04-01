e19936723c52fe04b1b5fca6b6909ac1f230b03b
Kaspar Bach Pedersen <kbp@digital-identity.dk>
2026-04-01 16:17:31 +0200

    Flyttet og centraliseret notifikationslogik for opgaveansvarlige til NotifyService
    Logikken til underretning af opgaveansvarlige er flyttet fra TasksController til NotifyService for at sikre ensartet håndtering og reducere kompleksiteten i controlleren. Notifikationer sendes nu automatisk ved både oprettelse og opdatering af opgaver, såfremt de er konfigureret til det.

--- src/main/java/dk/digitalidentity/service/NotifyService.java
+++ src/main/java/dk/digitalidentity/service/NotifyService.java
@@ -79,6 +79,39 @@ public class NotifyService {
 		}
 	}
 
+    public void notifyTaskResponsible(final Task task) {
+        if (task.getNotifyResponsible() == null || !task.getNotifyResponsible() || task.getResponsibleUsers().isEmpty()) {
+            return;
+        }
+        EmailTemplate template = emailTemplateService.findByTemplateType(EmailTemplateType.TASK_RESPONSIBLE);
+        if (!template.isEnabled()) {
+            log.info("Email template with type " + template.getTemplateType() + " is disabled. Email was not sent.");
+            return;
+        }
+        final String url = diSamlConfiguration.getSp().getBaseUrl() + "/tasks/" + task.getId();
+        final String objectName = task.getName();
+        final String link = "<a href=\"" + url + "\">" + url + "</a>";
+        for (User responsibleUser : task.getResponsibleUsers()) {
+            if (!StringUtils.hasLength(responsibleUser.getEmail())) {
+                continue;
+            }
+            String title = template.getTitle()
+                    .replace(EmailTemplatePlaceholder.RECEIVER_PLACEHOLDER.getPlaceholder(), responsibleUser.getName())
+                    .replace(EmailTemplatePlaceholder.OBJECT_PLACEHOLDER.getPlaceholder(), objectName)
+                    .replace(EmailTemplatePlaceholder.LINK_PLACEHOLDER.getPlaceholder(), link);
+            String message = template.getMessage()
+                    .replace(EmailTemplatePlaceholder.RECEIVER_PLACEHOLDER.getPlaceholder(), responsibleUser.getName())
+                    .replace(EmailTemplatePlaceholder.OBJECT_PLACEHOLDER.getPlaceholder(), objectName)
+                    .replace(EmailTemplatePlaceholder.LINK_PLACEHOLDER.getPlaceholder(), link);
+            eventPublisher.publishEvent(EmailEvent.builder()
+                    .message(message)
+                    .subject(title)
+                    .email(responsibleUser.getEmail())
+                    .templateType(template.getTemplateType())
+                    .build());
+        }
+    }
+
     public void notifyAboutInactiveUsers(Set<String> newlyInactiveUuids) {
         if (!newlyInactiveUuids.isEmpty()) {
             String email = settingsService.getString("inactiveResponsibleEmail", null);
