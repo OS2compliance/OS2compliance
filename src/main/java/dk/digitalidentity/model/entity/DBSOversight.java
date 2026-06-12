40059a7437fe309eb6394858c1b4248ec1b3dc35
abodigital <abo@digital-identity.dk>
2026-06-12 12:28:01 +0200

    Spotless

--- src/main/java/dk/digitalidentity/model/entity/DBSOversight.java
+++ src/main/java/dk/digitalidentity/model/entity/DBSOversight.java
@@ -13,7 +13,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "dbs_oversight")
