e3a95385bc5fb5ee5b591f933af508c50d90dced
friis jensen <friise2001@googlemail.com>
2026-07-20 15:25:49 +0200

    fixed migrations

--- src/main/resources/db/migration/V1_115__add_auditlog_table.sql
+++ src/main/resources/db/migration/V1_115__add_auditlog_table.sql
@@ -3,7 +3,8 @@ CREATE TABLE auditlog (
     created_timestamp       DATETIME,
     performer_uuid          VARCHAR(36),
     performer_name          VARCHAR(255),
-    entity_id               BIGINT,
+    entity_id               VARCHAR(36),
     entity_type             VARCHAR(255),
-    entity_name             VARCHAR(255)
+    entity_name             VARCHAR(255),
+    description             TEXT NULL
 );
--- src/main/resources/db/migration/V1_117__add_description_to_auditlog.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE auditlog ADD COLUMN description TEXT NULL;
-ALTER TABLE auditlog MODIFY COLUMN entity_id VARCHAR(64) NULL;
